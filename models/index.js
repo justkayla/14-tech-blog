a6614e76b11d84df59860167980ce92ced34ee74
Kayla Justice <kaylarjustice@gmail.com>
2022-10-26 18:13:54 -0500

    Building out model relationships in index.js file

--- models/index.js
+++ models/index.js
@@ -2,14 +2,44 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// User hasMany Post
+// a User can hasOne Post
+User.hasOne(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
-// Post belongsTo User
+// a User can hasMany Post
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',    // Does this delete all user posts?
+})
 
-// User hasMany Comment
+// a Post belongsTo one User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
-// Post hasMany Comment
+// a Post can hasOne Comment
+Post.hasOne(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
 
-// Comment belongsTo Post && User
+// a Post can hasMany Comment
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',    // Does this delete all comments on post?
+});
+
+// a Comment belongsTo one Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+// Need to define User-Comment relationship, or is that relationship defined through the Post route?
 
 module.exports = { User, Post, Comment };
