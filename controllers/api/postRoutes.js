80b84f01c73c4688b5c50522df403d58cad07737
Kayla Justice <kaylarjustice@gmail.com>
2022-10-26 20:55:53 -0500

    Building out postRoutes.js in controllers

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -6,6 +6,18 @@ const withAuth = require('../../utils/auth');
 // The `/api/posts` endpoint
 
 // CREATE a new post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const resp = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(resp);
+    } catch (err) {
+        res.status(400).json(err.message);
+    }
+});
 
 // UPDATE an existing post
     // Reload post
@@ -13,5 +25,24 @@ const withAuth = require('../../utils/auth');
     // Display any comments added
 
 // DELETE an existing post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const resp = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!resp) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.status(200).json(resp);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
